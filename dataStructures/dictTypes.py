dbb97d1948ace80b774f5a1e8382101b5186334e
Rob Spielman <rspielman@luc.edu>
2020-09-08 00:32:31 -0500

    [Enhancement] Add Data Structures Dictionary types: RangeDict, TupleDict

---
+++ dataStructures/dictTypes.py
@@ -0,0 +1,20 @@
+class RangeDict(dict):
+    def __getitem__(self, item):
+        if not isinstance(item, range):
+            for key in self:
+                if item in key:
+                    return self[key]
+            raise KeyError(item)
+        else:
+            return super().__getitem__(item)
+
+
+class TupleDict(dict):
+    def __getitem__(self, item):
+        if not isinstance(item, tuple):
+            for key in self:
+                if item in key:
+                    return self[key]
+            raise KeyError(item)
+        else:
+            return super().__getitem__(item)
